9c15150a15d37b606a1ece0225a2429d03968b27
ginpus <gintautas.pusinskas@gmail.com>
2021-08-14 11:31:34 +0300

    dapper select, insert, delete

--- DBconnDapper/Program.cs
+++ DBconnDapper/Program.cs
@@ -24,17 +24,59 @@ namespace DBconnDapper
             using var connection = new MySqlConnection(connectionString); // object represents connectivity
 
             //connection.Open(); //not neccessary with dapper
+            // ---------------SELECT---------------
+            var sqlSelect = "select * from customers2";
 
-            var sql = "select * from customers2";
+            var customers = connection.Query<Customer>(sqlSelect);
 
-            var customers = connection.Query<Customer>(sql);
-
-            connection.Open();
             foreach (var customer in customers)
             {
                 Console.WriteLine(customer);
             }
-            connection.Close();
+
+            // ---------------INSERT---------------
+            /*            var sqlInsert = @"INSERT INTO customers2 (first_name, last_name, email, street, city, state, age) VALUES(@first_name, @last_name, @email, @street, @city, @state, @age)";
+
+                        // single insert
+                        *//*            connection.Execute(sqlInsert, new
+                                    {
+                                        first_name = "Algis",
+                                        last_name = "Glamoris",
+                                        email = "alg.glam@yahoooo.com",
+                                        street = "5 Ciauduliu",
+                                        city = "Didelis",
+                                        state = "Alytus",
+                                        age = 100
+                                    });*//*
+                        var fakeCustomers = GeneratePersonsYield(10);
+
+                        //multiple inserts from IEnumerable (List)
+
+                        connection.Execute(sqlInsert, fakeCustomers); // automatically identifies, that given type is a list of objects*/
+
+            // ---------------DELETE---------------
+            /*            var sqlDelete = "DELETE FROM customers2 where customer_id = @customer_id";
+
+                        var rowsAffected = connection.Execute(sqlDelete, new { Customer_Id = 20 });
+
+                        Console.WriteLine(rowsAffected);*/
+        }
+
+        public static IEnumerable<Customer> GeneratePersonsYield(int customerCount)
+        {
+            for (var i = 0; i < customerCount; i++)
+            {
+                yield return new Customer
+                {
+                    First_Name = Faker.Name.First(),
+                    Last_Name = Faker.Name.Last(),
+                    Email = Faker.Internet.Email(),
+                    Street = Faker.Address.StreetAddress(),
+                    City = Faker.Address.City(),
+                    State = Faker.Address.UsState(),
+                    Age = Faker.RandomNumber.Next(1, 100)
+                };
+            }
         }
     }
 }
